d78bdcd05d2a25156cff9b67f5862632487df323
susannalassila <s1912801@ed.ac.uk>
2021-10-15 18:29:56 +0100

    Co-authored-by: EmilyOIG <EmilyOIG@users.noreply.github.com>

--- getIngredients/scrape.py
+++ getIngredients/scrape.py
@@ -2,25 +2,29 @@ import requests
 
 from bs4 import BeautifulSoup
 
-def getIngredientsFromPage(url):
-    page = requests.get(url)
-    
-    soup = BeautifulSoup(page.content, "html.parser")
-    ingredientSection = soup.find("section", class_="recipe__ingredients")
-    ingredients = ingredientSection.find_all("li", class_="list-item")
+class MeatSubstitution {
 
-    results = []
-    for i in ingredients:
-        results.append(i.text.split(" "))
+    def getIngredientsFromPage(url):
+        page = requests.get(url)
+        
+        soup = BeautifulSoup(page.content, "html.parser")
+        ingredientSection = soup.find("section", class_="recipe__ingredients")
+        ingredients = ingredientSection.find_all("li", class_="list-item")
 
-    #print(results)
-    return results
+        results = []
+        for i in ingredients:
+            results.append(i.text.split(" "))
 
-getIngredientsFromPage("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew")
+        #print(results)
+        return results
+
+    getIngredientsFromPage("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew")
 
 
-def containsMeat(ingredient):
-    return False
+    def containsMeat(ingredient):
+        return False
 
-def findSubstitute(meatType):
-    return None
+    def findSubstitute(meatType):
+        return None
+
+}
