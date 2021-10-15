23cb3b99734508c003674cc56b56e2eb153c5878
susannalassila <s1912801@ed.ac.uk>
2021-10-15 19:34:50 +0100

    Changes

--- getIngredients/scrape.py
+++ getIngredients/scrape.py
@@ -1,12 +1,23 @@
 import requests
-
+import os
+import numpy as np
+import pandas as pd
 from bs4 import BeautifulSoup
 
-class MeatSubstitution {
+class MeatSubstitution:
 
-    def getIngredientsFromPage(url):
+    def __init__(self):
+        self.substitutes = self.loadData()
+
+    def loadData(self):
+        path = os.path.join(os.getcwd(), "data", "Substitutes.csv")
+        df = pd.read_csv(path)
+        df.set_index('Ingredient', inplace=True)
+        print(df.head())
+        return df
+
+    def getIngredientsFromPage(self, url):
         page = requests.get(url)
-        
         soup = BeautifulSoup(page.content, "html.parser")
         ingredientSection = soup.find("section", class_="recipe__ingredients")
         ingredients = ingredientSection.find_all("li", class_="list-item")
@@ -18,13 +29,27 @@ class MeatSubstitution {
         #print(results)
         return results
 
-    getIngredientsFromPage("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew")
+    def function(self, ingredientsList):
+        result = []
+        
+        for ingredient in ingredientsList:
+            meat = self.findMeat(ingredient)
+            if meat != None:
+                result.append((meat,self.findSubstitute(meat)))
 
+        return result
 
-    def containsMeat(ingredient):
-        return False
-
-    def findSubstitute(meatType):
+    def findMeat(self, ingredient):
+        for word in ingredient:
+            if word in self.substitutes.index:
+                return word
         return None
 
-}
+    def findSubstitute(self, meatType):
+        substitute = self.substitutes.loc[meatType]['Substitute']
+        return substitute
+
+sub = MeatSubstitution()
+
+ing = sub.getIngredientsFromPage("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew")
+print(sub.function(ing))
