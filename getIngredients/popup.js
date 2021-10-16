854422e8669a1bdd075d981c612a853351919b3b
EmilyOIG <emlygghn@gmail.com>
2021-10-16 09:51:34 +0100

    Beginning javascript

---
+++ getIngredients/popup.js
@@ -0,0 +1,10 @@
+import DataFrame from 'dataframe.js';
+import DataFrame, { Row } from 'dataframe.js';
+
+
+var DataFrame = require('dataframe.js'.DataFrame);
+var DataFrame = dfjs.DataFrame;
+
+DataFrame.fromCSV('data/Substitutes.csv').then(df => df);
+
+df.show()
