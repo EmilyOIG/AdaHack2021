4700e1612f7d9414b5bf4a70b0cbd3b018e6c252
susannalassila <s1912801@ed.ac.uk>
2021-10-17 11:14:57 +0100

    More cleaning

--- veggieSwap/backend.js
+++ veggieSwap/backend.js
@@ -14,7 +14,6 @@ app.use(function(req, res, next) {
 
 function csvToList(res, callback) {
     var fileLocation = path.join(__dirname, 'data', 'Substitutes.csv');
-    //console.log(fileLocation);
 
     var csvData=[];
     fs.createReadStream(fileLocation)
@@ -22,12 +21,9 @@ function csvToList(res, callback) {
         .on('data', function(csvrow) {
             for (i=0;i<csvrow.length;i++){
                 csvData.push(csvrow[i].split(","));
-            }
-            //csvData.push(csvrow)   
+            } 
         })
         .on('end',function() {
-        //do something with csvData;   
-        //console.log(csvData);
         callback(csvData);  
         });
 }
@@ -48,7 +44,6 @@ function getIngredientsFromPage(url, res, callback) {
                 ingredients.push(liArr[i].split("<!-- -->")[1].split(" "));
             }
 
-            // console.log(ingredients);
             callback(ingredients);
         }
     });
@@ -78,15 +73,11 @@ function getSubsForURL(url, res) {
         });
 }
 
-// app.get("/", function(req, res) {
-//     getSubsForURL("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew", res)
-// });
 
 app.get("/", function(req, res) {
     let url = req.query.RecipleUrl;
     console.log(url);
     getSubsForURL(url, res)
-    //getSubsForURL("https://www.bbcgoodfood.com/recipes/beef-guinness-stew-bacon-dumplings", res)
 });
 
 let port = process.env.PORT;
@@ -96,35 +87,3 @@ port = 5000;
 app.listen(port, function() {
 console.log("Server started successfully");
 });
-
-// var testResult;
-// getSubsForURL("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew", function(results) {
-//     testResult = results;
-// });
-// console.log(testResult);
-
-
-// function betterFindSubstitute(url) {
-    
-//     var output = ""
-//     ingredientListList.forEach(ingredientList => {
-//         ingredientList.forEach(ingredient => {
-//             substitutes.forEach(element => {
-//                 if (element[0] == ingredient) {
-//                     //console.log(ingredient + ": " + element[1]);
-//                     output += ingredient + ":" + element[1] + ",";
-//                 }
-//             });
-//         });
-//     });
-//     console.log(output);
-//     return output;
-// }
-
-// console.log(getSubsForURL("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew"));
-
-// getIngredientsFromPage("https://www.bbcgoodfood.com/recipes/slow-cooker-beef-stew", function(ListofLists) {
-//     csvToList(function(csvData) {    
-//         findSubstitute(ListofLists, csvData);
-//         });
-// });
